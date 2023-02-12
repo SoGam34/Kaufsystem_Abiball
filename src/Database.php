62bd4f4f19d546980664b0f38e4588ed06376f99
SoGam34 <117866365+SoGam34@users.noreply.github.com>
2023-02-12 15:57:53 +0100

    Add files via upload

---
+++ src/Database.php
@@ -0,0 +1,86 @@
+<?php
+
+class Database
+{
+    private PDO $conn;
+
+    public function __construct(private string $host,
+                                private string $name,
+                                private string $user,
+                                private string $password)
+    {
+        $this->conn= $this->getConnection();
+    }
+        
+    public function getConnection(): PDO
+    {
+        $dsn = "mysql:host={$this->host};dbname={$this->name};charset=utf8";
+        
+        return new PDO($dsn, $this->user, $this->password, [
+            PDO::ATTR_EMULATE_PREPARES => false,
+            PDO::ATTR_STRINGIFY_FETCHES => false
+        ]);
+    }
+
+    public function insertUser(string $Email, string $UserPassword ): void
+    {
+        $sql = "INSERT INTO benutzer (email, passwort)
+        VALUES (:email, :passwort)";
+
+        $stmt = $this->conn->prepare($sql);
+
+        $stmt->bindValue(":email", $Email, PDO::PARAM_STR);
+        $stmt->bindValue(":passwort", $UserPassword, PDO::PARAM_STR);
+
+        $stmt->execute();
+    }
+
+    public function getUser(string $email): array | false
+    {
+        $sql = "SELECT *
+                FROM benutzer
+                WHERE email = :email";
+                
+        $stmt = $this->conn->prepare($sql);
+
+        $stmt->bindValue(":email",  $email, PDO::PARAM_STR);
+
+        $stmt->execute();
+        
+        $data = $stmt->fetch(PDO::FETCH_ASSOC);
+        
+        if ($data !== false) {
+        }
+        
+        return $data;
+    }
+    
+    public function ResetPasswort($email, string $newPasswort): void
+    {
+        $sql = "UPDATE benutzer
+                SET passwort = :passwort
+                WHERE email = :email";
+                
+        $stmt = $this->conn->prepare($sql);
+        
+        $stmt->bindValue(":email", $email, PDO::PARAM_STR);
+        $stmt->bindValue(":passwort", $newPasswort, PDO::PARAM_STR);
+        
+        $stmt->execute();
+    }
+    
+    public function delete($email): void
+    {
+        $sql = "DELETE FROM benutzer
+                WHERE email = :email";
+                
+        $stmt = $this->conn->prepare($sql);
+        
+        $stmt->bindValue(":email", $email, PDO::PARAM_STR);
+        
+        $stmt->execute();
+    }
+
+}
+
+?>
