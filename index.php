7af8428c6aaac9d6cb226339423ed4e5ebc64d2c
SoGam34 <117866365+SoGam34@users.noreply.github.com>
2023-02-14 22:46:22 +0100

    some bug fixes

--- index.php
+++ index.php
@@ -12,16 +12,30 @@ set_exception_handler("ErrorHandler::handleException");
 header("Content-type: application/json; charset=UTF-8");
 
 $parts = explode("/", $_SERVER["REQUEST_URI"]);
-
-if ($parts[1] != "loggingin"||$parts[1] != "register"||$parts[1] != "reseting") {
+echo "Vorbereitung erfolgreich";
+/*-------------------Erstellen aller Klassenobjeckte-------------*/
+$database = new Database("localhost", "abiball", "root", "root");
+echo "database erfolgreich";
+$UserHandling=new UserHandling();
+echo $UserHandling;/*= new UserHandling($database);
+echo "\nUser handling erfolgreich";exit;*/
+https://github.com/SoGam34/Kaufsystem_Abiball
+/*-------------------Bearbeiten der Anfrage-------------*/
+/*switch($parts[5])
+{git remote set-url origin {https://github.com/SoGam34/Kaufsystem_Abiball}
+    case "loggingin":
+        $UserHandling->checkLogin(); 
+    break;
+    case "register":
+        $UserHandling->createAcc();
+    break;
+    case "reseting":
+        $UserHandling->resetAcc();
+    break;
+    default:
     http_response_code(404);
     exit;
+    break;
 }
-
-//$id = $parts[2] ?? null;
-
-$database = new Database("localhost", "product_db", "root", "");
-
-$UserHandling = new UserHandling($database);
-
-$UserHandling->processRequest($_SERVER["REQUEST_METHOD"], $parts[1]);
+*/
+?>
