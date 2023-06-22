e7ef4fdaf7e40bcd551630eed10dc7b774162c35
Yantron <123463409+Yantron@users.noreply.github.com>
2023-06-22 14:36:12 +0200

    Update LoginRegistrierenSachen.js

--- Browser/LoginRegistrierenSachen.js
+++ Browser/LoginRegistrierenSachen.js
@@ -1,1 +1,11 @@
-
+asynch function dieVierPersoenlicheDatenAnRegister(vorname,nachname,klasse,email)
+{
+  const response = await fetch("https://abi24bws.de/Register", {
+      method: "POST", // or 'PUT'
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({"vorname":vorname,"nachname":nachname,"klasse":klasse,"email":email}),
+    });
+  const result = await response.json();
+}
