2054ede6a1dfc56a5f1d01a145efa6087427367a
SoGam34 <117866365+SoGam34@users.noreply.github.com>
2024-01-29 09:58:09 +0100

    Löschen von alten nicht mehr Benötigen Datein

--- Server/src/Tickets/DatabaseTickets.php
+++ Server/src/Tickets/DatabaseTickets.php
@@ -5,40 +5,7 @@ class DatabaseTickets
     public function __construct(private Security $sicher, private PDO $dbwrite, private PDO $dbreade)
     { 
     }
-
-    public function getAlleSitzplatze() : array {
-        $stmt = $this->dbreade->prepare(
-            "SELECT *
-             FROM sitzplatze;");
-
-        $stmt->execute();
-
-        $row = $stmt->fetchAll();
-        
-        return $row;
-    }
-
-    public function insertSitzplatze() : void {
-       try{
-
-        for( $i=0; $i<25; $i++)
-        {
-            
-            $stmt = $this->dbwrite->prepare(
-                "INSERT INTO sitzplatze (PersonID)
-                 VALUES ( :PersonID);"
-            );
-
-            $stmt->bindValue(":PersonID",  0);
-
-            $stmt->execute();
-        }
-    } catch (PDOException $e) {
-        echo json_encode(["Status" => "ERROR", "Message" =>  $e->getMessage()]);
-        exit;
-    }
-    }
-
+    
     public function setTicket(string $email, int $anzahl) {
         try{
             $stmt = $this->dbwrite->prepare(
